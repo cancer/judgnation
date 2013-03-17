7cae49df03f98239231922212754044862636bd1
geckotang <macky1207@gmail.com>
2013-03-17 23:45:40 +0900

    regardeを削除

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,6 @@
 module.exports = function(grunt){
-
-	var path = require('path');
-	var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
-
-	var folderMount = function folderMount(connect, point) {
-		return connect.static(path.resolve(point));
-	};
-
 	// load all grunt-plugin tasks
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
 	// init config
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -23,20 +14,11 @@ module.exports = function(grunt){
 		// setup localhost
 		// @url https://gist.github.com/konitter/5001702
 		connect: {
-			livereload: {
+			server: {
 				options: {
 					port: 9000,
-					middleware: function(connect, options) {
-						return [lrSnippet, folderMount(connect, '.')]
-					}
+					hostname: 'localhost'
 				}
-			}
-		},
-		// Configuration to be run (and then tested)
-		regarde: {
-			fred: {
-				files: ['index.html', 'css/style.css', 'js/all.js'],
-				tasks: ['livereload']
 			}
 		},
 		// concat JavasSript Files
@@ -78,6 +60,6 @@ module.exports = function(grunt){
 	});
 
 	// resiter tasks
-	grunt.registerTask('default', ['clean', 'compass', 'livereload-start', 'connect', 'regarde', 'concat', 'uglify', 'watch']);
+	grunt.registerTask('default', ['clean', 'compass', 'connect:server', 'concat', 'uglify', 'watch']);
 };
 
