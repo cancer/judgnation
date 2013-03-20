50f2e9a110c60d2935cc8d419adaebdd4eacab80
geckotang <macky1207@gmail.com>
2013-03-21 01:01:21 +0900

    expressを入れた。とりあえずテンプレート。

--- web.js
+++ web.js
@@ -1,7 +1,36 @@
-var express = require('express');
-var app = express.createServer();
-app.get('/', function(req, res) {
-	res.send('Hello World');
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./site/routes')
+  , user = require('./site/routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+	console.log(__dirname);
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/site/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, '/site/public')));
 });
-var port = process.env.PORT || 3000;
-app.listen(port);
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
